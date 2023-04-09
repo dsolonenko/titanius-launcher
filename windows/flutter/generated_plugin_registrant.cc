14f40ddb48769eae63d399e5e39c0207c467a795
Denis Solonenko <denis.solonenko@gmail.com>
2023-04-10 07:43:00 +1000

    Format time according to system settings

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -10,6 +10,7 @@
 #include <connectivity_plus/connectivity_plus_windows_plugin.h>
 #include <isar_flutter_libs/isar_flutter_libs_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
+#include <system_date_time_format/system_date_time_format_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   BatteryPlusWindowsPluginRegisterWithRegistrar(
@@ -20,4 +21,6 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("IsarFlutterLibsPlugin"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+  SystemDateTimeFormatPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SystemDateTimeFormatPluginCApi"));
 }
