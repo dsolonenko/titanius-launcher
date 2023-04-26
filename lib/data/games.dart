87bd47fb9a987a711785297b81de5e34fbbe61b7
Denis Solonenko <denis.solonenko@gmail.com>
2023-04-26 13:29:40 +1000

    Fix All Games collection

--- lib/data/games.dart
+++ lib/data/games.dart
@@ -121,41 +121,41 @@ Future<GameList> games(GamesRef ref, String systemId) async {
   final settings = await ref.watch(settingsProvider.future);
   final recentGames = await ref.watch(recentGamesProvider.future);
 
-  final system = systems.firstWhere((element) => element.id == systemId);
+  final system = systems.firstWhere((system) => system.id == systemId);
   final favouritesMap = {for (var favourite in settings.favourites) favourite.romPath: favourite.favourite};
 
-  for (var element in allGames) {
-    element.favorite = favouritesMap[element.romPath] ?? element.favorite;
+  for (var game in allGames) {
+    game.favorite = favouritesMap[game.romPath] ?? game.favorite;
   }
 
   switch (system.id) {
     case "favourites":
-      final games = allGames.where((element) => element.favorite).sortedBy((element) => element.name);
+      final games = allGames.where((game) => game.favorite).sortedBy((game) => game.name);
       final gamesInCollection = settings.uniqueGamesInCollections ? _uniqueGames(games) : games;
       return GameList(system, ".", gamesInCollection);
     case "recent":
       Map<String, int> recentGamesMap = {
         for (var item in recentGames) item.romPath: item.timestamp,
       };
-      final games = allGames.where((element) => recentGamesMap.containsKey(element.romPath)).toList();
+      final games = allGames.where((game) => recentGamesMap.containsKey(game.romPath)).toList();
       games.sort((a, b) => recentGamesMap[b.romPath]!.compareTo(recentGamesMap[a.romPath]!));
       final gamesInCollection = settings.uniqueGamesInCollections ? _uniqueGames(games) : games;
       return GameList(system, ".", gamesInCollection);
     case "all":
-      final games = _sortGames(settings, allGames);
-      final gamesInCollection = settings.uniqueGamesInCollections ? _uniqueGames(games) : games;
+      final gamesButNotFolders = allGames.where((game) => !game.isFolder).toList();
+      final games = settings.uniqueGamesInCollections ? _uniqueGames(gamesButNotFolders) : gamesButNotFolders;
+      final gamesInCollection = _sortGames(settings, games);
       return GameList(system, ".", gamesInCollection);
     default:
-      final games = _sortGames(settings, allGames.where((element) => element.system.id == system.id).toList());
+      final games = _sortGames(settings, allGames.where((game) => game.system.id == system.id).toList());
       return GameList(system, ".", games);
   }
 }
 
 List<Game> _uniqueGames(List<Game> allGames) {
   final roms = <String>{};
-  final uniqueGames = allGames.where((element) => !element.isFolder).toList();
-  uniqueGames.retainWhere((game) => roms.add("${game.system.id}/${game.name}"));
-  return uniqueGames;
+  allGames.retainWhere((game) => roms.add("${game.system.id}/${game.name}"));
+  return allGames;
 }
 
 List<Game> _sortGames(Settings settings, List<Game> allGames) {
