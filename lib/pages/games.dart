c6bd928ddcb6097d5e4783141799ca0ac5a545ae
Denis Solonenko <denis.solonenko@gmail.com>
2023-12-29 17:23:56 +1100

    UI fixes

--- lib/pages/games.dart
+++ lib/pages/games.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 
 import 'package:android_intent_plus/android_intent.dart';
 import 'package:collection/collection.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:fluttertoast/fluttertoast.dart';
